b357f95531c3529bb8ae12f04d046ccae7e36f17
Konain <konainraza105@gmail.com>
2024-04-03 00:02:11 +0500

    v1.0

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Quotes from "./Components/Quotes"
+import bg from "./assets/bg.jpg";
+
+function App() {
+  
+
+  return (
+<div id='mainPage' className='w-screen'>
+
+<Quotes/>
+</div>
+  )
+}
+
+export default App
---
+++ src/Components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./loading.css"
+function Loading() {
+  return (
+    
+    <div className="loader"></div> 
+
+        
+  )
+}
+
+export default Loading
