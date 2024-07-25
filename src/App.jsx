ea30ea8e81a8fa647da544a7f38887271fddfd00
Konain Raza <131591948+Konain-Raza@users.noreply.github.com>
2024-07-25 15:57:18 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Quotes from "./Components/Quotes"
-import bg from "./assets/bg.jpg";
+import React from 'react';
+import './App.css';
+import Quotes from './Components/Quotes';
+import bg from './assets/video.mp4';
 
 function App() {
-  
-
   return (
-<div id='mainPage' className='w-screen'>
-
-<Quotes/>
-</div>
-  )
+    <div id='mainPage' className='w-screen'>
+      <video src={bg} autoPlay loop muted></video>
+      <Quotes />
+    </div>
+  );
 }
 
-export default App
+export default App;
